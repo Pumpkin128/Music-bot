289f4910e6b54597c9d548b2cface0eb76ec3359
Pumpkin128 <74567305+Pumpkin128@users.noreply.github.com>
2021-07-14 01:20:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
 const fs = require('fs');
 const discord = require('discord.js');
 
